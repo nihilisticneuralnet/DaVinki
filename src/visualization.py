d7bc336d9a4c305adff210a3cd6bd08af222935e
Νιhιλιѕτιc Νευrαl Νετωοrk <138315505+nihilisticneuralnet@users.noreply.github.com>
2025-06-29 00:21:48 +0530

    Update visualization.py

--- src/visualization.py
+++ src/visualization.py
@@ -1,15 +1,18 @@
 import matplotlib.pyplot as plt
 
 def display_results(target_image, results):
-    fig, axes = plt.subplots(1,2, figsize=(15, 15))
+    fig, axes = plt.subplots(1, 2, figsize=(15, 15))
     
-    axes[0, 0].imshow(target_image)
-    axes[0, 0].set_title('Target Image')
-    axes[0, 0].axis('off')
+    axes[0].imshow(self.target_image)
+    axes[0].set_title('Target Image')
+    axes[0].axis('off')
     
-    axes[0, 1].imshow(results['mosaic'])
-    axes[0, 1].set_title('Mosaic Image')
-    axes[0, 1].axis('off')
-            
+    axes[1].imshow(results['mosaic'])
+    axes[1].set_title('Mosaic Image')
+    axes[1].axis('off')
+    
     plt.tight_layout()
     plt.show()
+
+
+
